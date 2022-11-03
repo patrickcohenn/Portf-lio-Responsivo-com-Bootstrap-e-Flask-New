ca5f3e7ed96edd18bdffc060adb61224b093a51c
Patrick Cohenn <patrickcohenn@gmail.com>
2022-11-03 07:33:21 -0300

    Menu responsivo.

--- static/script.js
+++ static/script.js
@@ -1,10 +1,13 @@
 /*Abre e fecha menu lateral em modo mobile */
-
+/*Pegando a class .menu-moblie */
 const menuMobile = document.querySelector ('.menu-mobile')
 const body = document.querySelector('body')
 
+/* if ternário no qual ele vai conferi se o icon esta list ou x*/
 menuMobile.addEventListener('click', () =>{ 
     menuMobile.classList.contains("bi-list")            /*If ternário*/
     ? menuMobile.classList.replace("bi-list", 'bi-x')   /*If ternário*/
     : menuMobile.classList.replace("bi-x", "bi-list");  /*If ternário*/
+    body.classList.toggle("menu-nav-active")          /*Vai add una class a tag no body*/
 })
+
