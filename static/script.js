91b55904dfe758f5627a81ec7cdd14d927209fad
Patrick Cohenn <patrickcohenn@gmail.com>
2022-11-08 10:48:24 -0300

    Animação Scroll  Thi Code  youtube
    Link 08-11-2022
    
    https://www.youtube.com/watch?v=Bhpw7d_NNYM&list=PLR8JXremim5DU70e3x_rYhClgMTzTyv4m&index=11

--- static/script.js
+++ static/script.js
@@ -22,3 +22,21 @@ navItem.forEach(item => {
         }
     })
 })
+
+const item = document.querySelectorAll("[data-anime]");
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+    item.forEach((eLement) => {
+        if (windowTop > eLement.offsetTop) {
+            eLement.classList.add("animate");
+        } else{
+            eLement.classList.remove("animate");
+        }
+    })
+}
+
+animeScroll()
+
+window.addEventListener("scroll", () => {
+    animeScroll();
+})
